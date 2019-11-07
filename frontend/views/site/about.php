cedb2b8bc40c30ee8bbf6b251a9a7d9f4dc330dd
Toan <dvtoan.kientruc@gmail.com>
2019-11-07 23:26:41 +0700

    last

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -13,4 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>This is the About page. You may modify the following file to customize its content:</p>
 
     <code><?= __FILE__ ?></code>
+    dsa dhlhsaldh
+
+    lshadlhs dsa ds ad
 </div>
