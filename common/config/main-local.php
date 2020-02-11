a063f6de5958d0d3d31ea7dc26e0b9b51d0e0b22
Your Name <you@example.com>
2020-02-11 08:47:08 +0700

    change database

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=doan',
+            'dsn' => 'mysql:host=localhost;dbname=game_new',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
